092df9d0da89b364381635e019a56f77f022dc23
Mali Akmanalp <mali@akmanalp.com>
2015-04-28 12:15:30 -0400

    Abstract out common file types

--- file_types.py
+++ file_types.py
@@ -1,4 +1,15 @@
-class Ecomplexity(object):
+class FileType(object):
+
+    def check_field_mappings(self, mappings):
+        return set(self.REQUIRED) - set(mappings)
+
+    def check_fields(self, field_mappings, df):
+        """Returns missing fields, extra fields"""
+        return (set(field_mappings.values()) - set(df.columns),
+                set(df.columns) - set(field_mappings.values()))
+
+
+class Ecomplexity(FileType):
     """Format dumped out from the `ecomplexity` command in STATA."""
 
     REQUIRED = {
@@ -17,13 +28,18 @@ class Ecomplexity(object):
         "coi": float,
     }
 
-    def check_field_mappings(self, mappings):
-        return set(self.REQUIRED) - set(mappings)
 
-    def check_fields(self, field_mappings, df):
-        """Returns missing fields, extra fields"""
-        return (set(field_mappings.values()) - set(df.columns),
-                set(df.columns) - set(field_mappings.values()))
+class CPY(FileType):
+    """Location - entity - year."""
 
+    REQUIRED = {
+        "location": str,
+        "entity": str,
+        "year": int,
+        "value": float,
+    }
 
-file_types = {"ecomplexity": Ecomplexity()}
+file_types = {
+    "ecomplexity": Ecomplexity(),
+    "cpy": CPY()
+}
